9d2484f22b6668f29b8cd32ad4432100acb6f975
Kuske <lucky.777yusuke@gmail.com>
2017-06-19 15:51:34 +0900

    タブの数をそろえるようにした(要修正)

---
+++ CS-CodeEditor/EditCode.cs
@@ -0,0 +1,109 @@
+﻿using System;
+// using Kuske.NewComers.KeyWordCheck;
+
+namespace Kuske.NewComers.CodeEditor
+{
+    class Indent
+    {
+
+        static public int GetTab_Num(string text, int Line_Num)
+        {
+            int ans = 0;
+
+            int NowLine = 1;
+            for (int a = 0; a < text.Length; a++)
+            {
+                if (NowLine == Line_Num)
+                {
+                    for (int b = 0; ; b++)
+                    {
+                        if (text[a + b] != '\t') break;
+                        else ans++;
+                    }
+                    return ans;
+                }
+                if (text[a] == '\n') NowLine++;
+            }
+            return 0;
+        }
+
+        static public string SetAllIndent(string Code)
+        {
+
+            // 現在の行で必要なタブの数
+            int NowIndent = 0;
+            int NowLineTab = 0;
+
+            // 配列の要素数
+            for (int a = 0; a < Code.Length; a++)
+            {
+                if (Code[a] == '\t')
+                {
+                    NowLineTab++;
+
+                    if (a < Code.Length - 1)
+                    {
+                        
+                        if (NowIndent < NowLineTab)
+                        {
+                            Code = Code.Remove(a, 1);
+                        }
+
+                        if (Code[a + 1] == '}')
+                        {
+                            Code = Code.Remove(a, 1);
+                        }
+                    }
+                }
+
+                if (Code[a] == '\n')
+                {
+                    NowLineTab = 0;
+                    if (a + 1 < Code.Length)
+                    {
+
+                        // 最初のインデントを確認  
+                        for (int indent_num = 0; a + indent_num <= Code.Length; indent_num++)
+                        {
+                            if (Code[a + indent_num + 1] != '\t')
+                            {
+                                if (indent_num <= NowIndent)
+                                {
+                                    // インデントを追加
+                                    for (int i = 0; i < NowIndent - indent_num; i++)
+                                    {
+                                        Code = Code.Insert(a + 1, "\t");
+                                    }
+                                }
+                                else
+                                {
+                                    // タブを消す
+                                    Code = Code.Remove(a + 1, indent_num - NowIndent);
+                                }
+                                break;
+                            }
+
+                            // インデントの更新
+                            if (Code[a + 1] == '{') NowIndent++;
+                            if (Code[a + 1] == '}') NowIndent--;
+                        }
+                    }
+                    else
+                    {
+                        break;
+                    }
+
+                }
+                else
+                {
+                    // インデントの更新
+                    if (Code[a] == '{') NowIndent++;
+                    if (Code[a] == '}') NowIndent--;
+                }
+
+            }
+
+            return Code;
+        }
+    }
+}
--- CS-CodeEditor/Form1.cs
+++ CS-CodeEditor/Form1.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Kuske.NewComers.CodeEditor;
+using System.IO;
+using System.Diagnostics;
 
 namespace CS_CodeEditor
 {
@@ -15,6 +18,50 @@ namespace CS_CodeEditor
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // インデントをそろえる
+            {
+                int Now_Pos = richTextBox1.SelectionStart;
+                int Now_Line = richTextBox1.GetLineFromCharIndex(richTextBox1.SelectionStart);
+
+                richTextBox1.Text = Indent.SetAllIndent(richTextBox1.Text);
+                richTextBox1.Text = Indent.SetAllIndent(richTextBox1.Text);
+
+                richTextBox1.SelectionStart = Now_Pos + Indent.GetTab_Num(richTextBox1.Text, Now_Line);
+                
+                if(richTextBox1.Text.Length > 0)
+                {
+                    if (richTextBox1.Text[Math.Min(richTextBox1.SelectionStart, Math.Max(0, richTextBox1.Text.Length - 1))] == '}')
+                        --richTextBox1.SelectionStart;
+                }
+            }
+
+            // 色の変更
+            {
+                string[] SearchWord = {
+                      "using"
+                    , "string"
+                };
+
+                for(int a = 0; a < SearchWord.GetLength(2); a++)
+                {
+                    int FoundIndex = richTextBox1.Text.IndexOf(SearchWord[a]);
+                    while(0 <= FoundIndex)
+                    {
+                        richTextBox1.Select(FoundIndex, SearchWord[a].Length);
+                        richTextBox1.SelectionColor = Color.Blue;
+                        
+                        if (FoundIndex + SearchWord[a].Length < )
+                    }
+                }
+
+            }
+
+            richTextBox1.Font = new Font("Consolas", 14);
+
         }
     }
 }
